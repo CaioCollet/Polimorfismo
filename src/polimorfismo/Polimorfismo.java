71e2b1248afd13e79c78fe26c71468f683613456
CAIO FELIPE ALVES COLLET <caio.collet@alunos.sc.senac.br>
2022-09-19 20:36:09 -0300

    Polimorfismo

---
+++ src/polimorfismo/Polimorfismo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author caio.collet
+ */
+public class Polimorfismo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora ();
+        System.out.println(calc.somar(2, 3, 6));
+        
+        
+    
+    }
+    
+}
